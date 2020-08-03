84f4ed66786a163fe10320b42c1394aeee725dd5
Jakov Jelenić <jakov.jelenic@fer.hr>
2020-08-03 21:25:00 +0200

    basic player movements
    player movements for pc, clamp speed and rotation
    Using rb

---
+++ Assets/Scripts/PlayerScripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform player;
+
+    // Use this for initialization
+    void Start () {
+        transform.position = new Vector3(player.position.x, player.position.y, player.position.z - 10f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(player.position.x, player.position.y,player.position.z - 10f);
+    }
+
+}
---
+++ Assets/Scripts/PlayerScripts/ShipMovement.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.Experimental.UIElements;
+
+public class ShipMovement : MonoBehaviour {
+
+    //public Joystick joystickrotate;
+    //public Joystick joystickspeed;
+    private Rigidbody2D rb;
+    private float thrust;
+    private float maxVelocity;
+    private float timetillfire;
+    public Transform firepoint;
+    public GameObject Bullets;
+    //public Button Thrust;
+
+
+
+
+
+    Vector2 mouseWorldPosition;
+    Vector2 direction;
+
+    // Use this for initialization
+    void Start() {
+        thrust = 20f;
+        rb = GetComponent<Rigidbody2D>();
+        maxVelocity = 80;
+        timetillfire = 0.2f;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        /*timetillfire -= Time.deltaTime;
+        if (joystickrotate.Vertical != 0 && joystickrotate.Horizontal != 0)
+        {
+            float angle = Mathf.Atan2(joystickrotate.Horizontal, joystickrotate.Vertical) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, -angle);
+        }
+        if (joystickspeed.Horizontal !=0 && joystickspeed.Vertical != 0)
+        {
+            float angleSpeed = Mathf.Atan2(joystickspeed.Horizontal, joystickspeed.Vertical) * Mathf.Rad2Deg;
+            Vector3 dir = Quaternion.Euler(0f, 0f, -angleSpeed)*Vector3.up;
+            rb.AddForce(dir * thrust * Time.deltaTime);
+        }*/
+
+        ClampVelocity();
+
+        //float yAxis = Input.GetAxis("Horizontal");
+        //float xAxis = Input.GetAxis("Vertical");
+
+        if (Application.platform != RuntimePlatform.Android)
+        {
+            if (Input.GetKey("w"))
+            {
+                Vector2 force = Vector3.up * thrust;
+                rb.AddForce(force);
+            }
+
+            if (Input.GetKey("s"))
+            {
+                Vector2 force = Vector3.down * thrust;
+                rb.AddForce(force);
+                //Debug.Log("s");
+            }
+
+
+            if (Input.GetKey("a"))
+            {
+                Vector2 force = Vector3.left * thrust;
+                rb.AddForce(force);
+            }
+
+            if (Input.GetKey("d"))
+            {
+                Vector2 force = Vector3.right * thrust;
+                rb.AddForce(force);
+            }
+            //rotate to mouse
+            mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            // get direction you want to point at
+            direction = (mouseWorldPosition - (Vector2)transform.position).normalized;
+
+            // set vector of transform directly
+            transform.up = direction;
+        }
+
+
+
+        /*if (timetillfire<=0)
+        {
+            Shoot();
+            timetillfire = 0.1f;
+        }*/
+
+    }
+
+    private void ClampVelocity(){
+        float x = Mathf.Clamp(rb.velocity.x, -maxVelocity, maxVelocity);
+        float y = Mathf.Clamp(rb.velocity.y, -maxVelocity, maxVelocity);
+        rb.velocity = new Vector2(x, y);
+        //Debug.Log(y.ToString());
+    }
+
+    void Shoot()
+    {
+        Instantiate(Bullets, firepoint.position,firepoint.rotation);
+    }
+
+
+}
