b1595f0c7510dee4f2378d69b5c92bdc0d578196
Jakov Jelenić <jakov.jelenic@fer.hr>
2020-08-04 20:03:41 +0200

    basic fireing
    collision doesnt work

---
+++ Assets/Scripts/PlayerScripts/BulletFire.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletFire : MonoBehaviour
+{
+
+    public float BulletForce;
+    private float maxVelocity;
+    private Rigidbody2D rb;
+    public static bool canHit;
+    public GameObject explosion;
+    private Transform bullet;
+
+    // Use this for initialization
+    void Start()
+    {
+        BulletForce = 90;
+        maxVelocity = 500;
+        rb = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, 5);
+        rb.AddForce(transform.up * BulletForce);
+        bullet = GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ClampVelocity();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        //Debug.Log("collision");
+
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            Destroy(gameObject, 0.0f);
+        }
+        else if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("hit an enemy");
+            Destroy(gameObject, 0.0f);
+        }
+
+
+    }
+
+
+    private void OnDestroy()
+    {
+        Instantiate(explosion, bullet.position, bullet.rotation);
+    }
+
+    private void ClampVelocity()
+    {
+        float x = Mathf.Clamp(rb.velocity.x, -maxVelocity, maxVelocity);
+        float y = Mathf.Clamp(rb.velocity.y, -maxVelocity, maxVelocity);
+        rb.velocity = new Vector2(x, y);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        Debug.Log("collision");
+
+        if (collision.gameObject.tag == "Player")
+        {
+            Physics.IgnoreCollision(collision.collider, GetComponent<Collider>());
+        }
+        else if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("hit an enemy");
+            Destroy(gameObject, 0.0f);
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/ShipFire.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipFire : MonoBehaviour
+{
+
+    private float timetillfire;
+    public Transform firepoint;
+    public GameObject Bullets;
+    private bool autofire;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timetillfire = 0.2f;
+        autofire = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timetillfire -= Time.deltaTime;
+
+        if (Input.GetKeyDown("space"))
+        {
+            if (autofire)
+            {
+                autofire = false;
+            }
+            else
+            {
+                autofire = true;
+            }
+        }
+
+        if (timetillfire<=0 && autofire)
+        {
+            Shoot();
+            timetillfire = 0.2f;
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(Bullets, firepoint.position, firepoint.rotation);
+    }
+}
