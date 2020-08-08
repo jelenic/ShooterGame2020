504fbeeeb7075ffcfa2042d40bd446e4fbb8ce96
petar19 <petar.lazic@fer.hr>
2020-08-08 20:06:24 +0200

    added enemy turret

---
+++ Assets/Scripts/EnemyScripts/Fired.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fired : MonoBehaviour
+{
+    public float force;
+    private float maxVelocity;
+    private Rigidbody2D rb;
+    public float lifeDuration;
+    public GameObject explosion;
+    private Transform bullet;
+
+    // Use this for initialization
+    void Start()
+    {
+        force = 3;
+        maxVelocity = 100;
+        rb = GetComponent<Rigidbody2D>();
+        lifeDuration = 5f;
+        Destroy(gameObject, lifeDuration);
+        rb.AddForce(transform.up * force);
+        bullet = GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ClampVelocity();
+    }
+
+
+    private void OnDestroy()
+    {
+        Instantiate(explosion, bullet.position, bullet.rotation);
+    }
+
+    private void ClampVelocity()
+    {
+        float x = Mathf.Clamp(rb.velocity.x, -maxVelocity, maxVelocity);
+        float y = Mathf.Clamp(rb.velocity.y, -maxVelocity, maxVelocity);
+        rb.velocity = new Vector2(x, y);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject, 0.0f);
+        Debug.LogFormat("hit:{0}", collision.gameObject.tag);
+        
+
+    }
+}
---
+++ Assets/Scripts/EnemyScripts/RotateTowardsTarget.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class RotateTowardsTarget : MonoBehaviour
+{
+    public float speed;
+    public float range;
+    public Quaternion defaultRotation;
+    public Transform target;
+    public GameObject bullet;
+
+    // Use this for initialization
+    void Start () {
+        speed = 10f;
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        range = 13f;
+        defaultRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector2.Distance(transform.position, target.position) <= range)
+        {
+            Vector2 direction = target.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+            Instantiate(bullet, transform.position, transform.rotation);
+
+        }
+        else
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, defaultRotation, speed * Time.deltaTime);
+        }
+        
+	}
+}
