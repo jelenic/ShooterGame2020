869cf1a76dff76c993191363cab17f11165a67ed
Jakov Jelenić <jakov.jelenic@fer.hr>
2020-08-08 16:56:16 +0200

    Added semiManual Script for enemy movement
    need to change it to work with kinematic rb (this only works with dynamic)

---
+++ Assets/Scripts/EnemyScripts/CustomAI.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class CustomAI : MonoBehaviour
+{
+
+    public Transform target;
+    private float speed;
+    private float nextWaypontDistance;
+    private float AngleSpeed;
+
+    Path path;
+    int currentWaypoint;
+    bool reachedEndOfPath;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 400f;
+        nextWaypontDistance = 3f;
+        currentWaypoint = 0;
+        reachedEndOfPath = false;
+        AngleSpeed = 20;
+
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, 0.5f);
+
+
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (path == null)
+        {
+            return;
+        }
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+        Debug.Log("Adding force" + force.ToString());
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+        if (distance < nextWaypontDistance)
+        {
+            currentWaypoint += 1;
+        }
+
+
+
+        //rotate towards player
+        Vector2 directionPlayer = target.position - transform.position;
+        float angle = Mathf.Atan2(directionPlayer.y, directionPlayer.x) * Mathf.Rad2Deg - 90;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, AngleSpeed * Time.deltaTime);
+    }
+}
