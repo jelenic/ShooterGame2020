abf891a36a99ad5e18726f332db46878f94ce877
petar19 <petar.lazic@fer.hr>
2020-08-09 20:53:29 +0200

    turret changes
    Added turrets to prefabs and set their layer to Walls for A*

--- Assets/Scripts/EnemyScripts/FiredHoming.cs
+++ Assets/Scripts/EnemyScripts/FiredHoming.cs
@@ -28,7 +28,7 @@ public class FiredHoming : MonoBehaviour
 
     
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag != "Projectile")
         {
