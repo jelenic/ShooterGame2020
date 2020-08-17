b2a6cad6ce7df4fa235acb7bcf20f9e2f62910b3
Jakov Jelenić <jakov.jelenic@fer.hr>
2020-08-17 19:35:23 +0200

    weaponSwitch, cleanUp

--- Assets/Scripts/PlayerScripts/ShipFire.cs
+++ Assets/Scripts/PlayerScripts/ShipFire.cs
@@ -7,9 +7,19 @@ public class ShipFire : MonoBehaviour
 
     private float timetillfire;
     public Transform firepoint;
-    public GameObject Bullets;
     private bool autofire;
     private Stats stats;
+    private int activeWeapon;
+    private GameObject w1;
+    private GameObject w2;
+
+
+
+
+
+
+    public GameObject Bullets;
+    public GameObject MiniLaser;
 
 
     // Start is called before the first frame update
@@ -18,6 +28,10 @@ public class ShipFire : MonoBehaviour
         stats = GetComponent<Stats>();
         timetillfire = stats.rateOfFire;
         autofire = false;
+
+        activeWeapon = 1;
+        w1 = Bullets;
+        w2 = MiniLaser;
         
     }
 
@@ -43,11 +57,27 @@ public class ShipFire : MonoBehaviour
             Shoot();
             timetillfire = stats.rateOfFire;
         }
+
+        if (Input.GetKeyDown("1"))
+        {
+            activeWeapon = 1;
+        }
+        else if (Input.GetKeyDown("2"))
+        {
+            activeWeapon = 2;
+        }
     }
 
     void Shoot()
     {
-        Instantiate(Bullets, firepoint.position, firepoint.rotation).GetComponent<BulletFire>().damageModifier = stats.calculateFinalDmgModifier();
+        if (activeWeapon == 1)
+        {
+            Instantiate(w1, firepoint.position, firepoint.rotation).GetComponent<BulletFire>().damageModifier = stats.calculateFinalDmgModifier();
+        }
+        else if (activeWeapon == 2)
+        {
+            Instantiate(w2, firepoint.position, firepoint.rotation).GetComponent<BulletFire>().damageModifier = stats.calculateFinalDmgModifier();
+        }
     }
 
 }
