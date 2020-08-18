8a95c6414c96ed63ec85f56e0cc461c412bf0933
petar19 <petar.lazic@fer.hr>
2020-08-18 19:22:41 +0200

    added preload scene and gamemanager

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -6,7 +6,13 @@ public class Collectible : MonoBehaviour
 {
 
     public string itemId;
+    public GameObject gameManager;
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager");
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -14,6 +20,8 @@ public class Collectible : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             Debug.LogFormat("player just collected item: {0}", itemId);
+            gameManager.GetComponent<Inventory>().addItem(itemId);
+
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DontDestroy : MonoBehaviour
+{
+    public int sceneToLoad;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (sceneToLoad == 0) sceneToLoad = 1;
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+}
