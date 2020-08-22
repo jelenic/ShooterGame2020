a820eecf8423164421e7d95c8f65fe03b969ad53
Jakov Jelenić <jakov.jelenic@fer.hr>
2020-08-22 19:59:13 +0200

    weaponIsNowAGameObject

---
+++ Assets/WeaponFire.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponFire : MonoBehaviour
+{
+    public GameObject prefab;
+    private Stats stats;
+
+    private void Start()
+    {
+        stats = gameObject.GetComponentInParent<Stats>();
+    }
+    public void Shoot()
+    {
+        Instantiate(prefab, transform.position, transform.rotation).GetComponent<PlayerFiredBullet>().damageModifier = stats.calculateFinalDmgModifier();
+    }
+}
