a6d8fdafc7fd5657ced1af0fa1758299a453d65a
petar19 <petar.lazic@fer.hr>
2020-09-19 18:06:58 +0200

    fix to cooldown callbacks

--- Assets/Scripts/PlayerScripts/CooldownUI.cs
+++ Assets/Scripts/PlayerScripts/CooldownUI.cs
@@ -8,21 +8,31 @@ public class CooldownUI : MonoBehaviour
     public Image module_cdbar;
     public Image special_weapon_cdbar;
 
+    private GameObject player;
+    private ModuleScript ms;
+    private SpecialWeaponScript sws;
+
     private void Awake()
     {
-        Invoke("lol", 0.3f);
+        StartCoroutine(addCallbacks());
     }
 
-    void lol()
+    private IEnumerator addCallbacks()
     {
-        GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<ModuleScript>().OnCooldownChangedCallback += module_change;
-        GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<SpecialWeaponScript>().OnCooldownChangedCallback += special_weapon_change;
+        yield return new WaitForSeconds(0.3f);
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        ms = player.GetComponentInChildren<ModuleScript>();
+        sws = player.GetComponentInChildren<SpecialWeaponScript>();
+
+        ms.OnCooldownChangedCallback += module_change;
+        sws.OnCooldownChangedCallback += special_weapon_change;
     }
 
     private void OnDestroy()
     {
-        GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<ModuleScript>().OnCooldownChangedCallback -= module_change;
-        GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<ModuleScript>().OnCooldownChangedCallback -= special_weapon_change;
+        ms.OnCooldownChangedCallback -= module_change;
+        sws.OnCooldownChangedCallback -= special_weapon_change;
     }
 
     private void module_change(float filled)
