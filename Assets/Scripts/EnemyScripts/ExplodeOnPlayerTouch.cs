bdc0c29ea5b4e95d0ddc2adb15cfa25373a91c63
Jakov Jelenić <jakov.jelenic@fer.hr>
2020-09-29 13:41:17 +0200

    finished kamikazee, fixed minor bugs

---
+++ Assets/Scripts/EnemyScripts/ExplodeOnPlayerTouch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeOnPlayerTouch : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            Destroy(gameObject, 0f);
+            collision.collider.gameObject.GetComponent<Damageable>().DecreaseHP(10, DamageType.Physical);
+        }
+    }
+}
